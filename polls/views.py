ac2b6c15f6f35d74b119141ef9b736f36b41cfd6
Aleph Melo <alephmelo@icloud.com>
2015-08-25 16:19:58 -0300

    Finish the fourth part.

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,41 @@
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+# Import your models here
+from .models import Question, Choice
 
 
-def index(request):
-    lql = Question.objects.order_by('pub_date')[:5]
-    context = {'lql': lql}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lql'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, q_id):
-    question = get_object_or_404(Question, pk=q_id)
-    return render(request, 'polls/detail.html', {'q': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    context_object_name = 'q'
 
 
-def results(request, q_id):
-    return HttpResponse('You are in the results of %s' % q_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, q_id):
-    return HttpResponse('You are voting on %s' % q_id)
+    p = get_object_or_404(Question, pk=q_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'q': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
