0b6de20f53be4e4367f2722feb858ecf5ee94d77
Aleph Melo <alephmelo@icloud.com>
2015-08-25 13:57:22 -0300

    Some changes in the views.

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
+from .models import Question
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    lql = Question.objects.order_by('pub_date')[:5]
+    context = {'lql': lql}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, q_id):
+    return HttpResponse('You are in the detail of %s' % q_id)
+
+
+def results(request, q_id):
+    return HttpResponse('You are in the results of %s' % q_id)
+
+
+def vote(request, q_id):
+    return HttpResponse('You are voting on %s' % q_id)
